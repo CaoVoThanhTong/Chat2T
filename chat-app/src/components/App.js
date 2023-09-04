5b95654f30f8456c8f6ad11724253af91f6beb20
Cao Võ Thanh Tòng <thanhtong2407@gmail.com>
2023-09-04 22:40:20 +0700

    còn firebase

--- chat-app/src/components/App.js
+++ chat-app/src/components/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Messenger from './Messenger';
